0d836188503dedc805302b55ce42c73dda265e51
Brenden <52762708+brendenmoore@users.noreply.github.com>
2021-01-12 19:56:42 -0500

    initialize angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { LoginComponent } from './login/login.component';
+import { PasswordResetComponent } from './password-reset/password-reset.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { WriteComponent } from './write/write.component';
+import { NoteListComponent } from './note-list/note-list.component';
+import { SettingsComponent } from './settings/settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    LoginComponent,
+    PasswordResetComponent,
+    PageNotFoundComponent,
+    WriteComponent,
+    NoteListComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
