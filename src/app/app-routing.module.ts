a701b69dd3c87ea94b9ca56da6074106f7159b44
Brenden <52762708+brendenmoore@users.noreply.github.com>
2021-01-12 20:22:43 -0500

    initial app layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
+import { WriteComponent } from './write/write.component';
 
 const routes: Routes = [
-  {path: "", component: LandingComponent}
+  {path: "", component: LandingComponent},
+  {path: "write", component: WriteComponent}
 ];
 
 @NgModule({
