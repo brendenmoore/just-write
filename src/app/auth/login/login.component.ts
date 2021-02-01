cb522e5cf2ce388232866f5992e1011ab95f18d8
Brenden <52762708+brendenmoore@users.noreply.github.com>
2021-02-01 15:51:40 -0500

    modify loginForm

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  isLoading: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onLogin(form: NgForm) {
+    console.log(form.value);
+  }
 }
